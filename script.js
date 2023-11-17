2217e224c978988a6cdd5251a3628c18c7118766
Khalil <khalilaliev0@gmail.com>
2023-11-17 22:54:49 +0100

    five digit concatenation

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstInput = prompt('Enter first input');
+let secondInput = prompt('Enter second input');
+let thirdInput = prompt('Enter third input');
+
+let digitString = firstInput + " " + secondInput + " " + thirdInput;
+console.log('Result:', digitString);
+
+let fiveDigitNumber = prompt('Enter a five-digit number:');
+let digitSplit = fiveDigitNumber.split('') .join(' ');
+
+console.log('Concatenation:', digitSplit);
+
